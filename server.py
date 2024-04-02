8a59c3f7d760beeb5b8ddcd2461bafbcbb7fd54c
Fazel Gheibi <fazelgheibi2001@gmail.com>
2024-04-02 16:39:34 +0330

    init

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket
 import pickle
 
+
 def start_server():
     HOST = '127.0.0.1'
     PORT = 8885
@@ -12,13 +13,16 @@ def start_server():
         print("server listening on port ", PORT)
 
         while True:
-            con, addr = s.accept()
-            with con:
-                while True:
-                    data = con.recv(1024)
-                    signal = pickle.loads(data)
-                    print('signal_type: ', signal.signal_type)
-                    print('value: ', signal.value)
+            try:
+                con, addr = s.accept()
+                with con:
+                    while True:
+                        data = con.recv(1024)
+                        signal = pickle.loads(data)
+                        print('signal_type: ', signal.signal_type)
+                        print('value: ', signal.value)
+            except:
+                print("client disconnected")
 
 
 if __name__ == '__main__':
